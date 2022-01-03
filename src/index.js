c9bf139ae3200e890e63208d2d692c4ab5374d10
saud <gomail2017@gmail.com>
2022-01-03 09:40:00 +0500

    deleting sass files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
-import "./styles/custom.scss";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
